205a53d46d3fbfdaca0152a03a5fe756f374a2ec
Felipe T <felipe.at.1989@gmail.com>
2013-11-28 10:03:03 -0200

    Esboço Interrupção AD

--- esboco.asm
+++ esboco.asm
@@ -1,7 +1,23 @@
+SomaFN:		3 posições
+
+SQuadFN:	4 posições
+
+amostraAD:	2 posições
 
 
 IntTimer:
 
-IntAD:
+IntAD:	;Passará o valor da conversão AD para a variável amostraAD
+	MOVF	amostraAD, w
+	ADDWF	SomaFN, f
+	MOVF	amostraAD+1, w
+	SKPNC	
+	ADDLW	1
+	SKPC
+	ADDWF	SOMAFN+1, f
+	SKPNC
+	INCF	SOMAFN+2, f
+	
+
 
 
